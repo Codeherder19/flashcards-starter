9552466ba4465d9144f2c8f859853652236a4b43
Brian Forbes <brianandrewf81@gmail.com>
2020-12-03 20:36:17 -0700

    Edit index.js file and finish game class to begin testing gameplay

--- test/Game-test.js
+++ test/Game-test.js
@@ -8,22 +8,15 @@ const Game = require('../src/Game');
 
 describe('Game', () => {
 
-  it('should keep track of current round', () => {
-    let game = new Game()
-    expect(game.currentRound).to.equal(0);
-    game.startGame();
-    expect(game.currentRound).to.equal(1);
-  });
-
   it('should put cards in deck', () => {
     let game = new Game();
-    game.createCards();
+    game.startGame();
     expect(game.deck.length).to.deep.equal(30);
   });
 
   it('should be able to instantiate a deck of flash cards', () => {
     let game = new Game();
-    game.createCards();
+    game.startGame();
     expect(game.deck.length).to.deep.equal(30);
   });
 
